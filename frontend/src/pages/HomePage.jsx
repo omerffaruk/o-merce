58333fdd3222dde7d4bf011486db67135c5124a4
omer <omer@idevu.io>
2022-03-20 19:13:41 +0000

    feat: home page

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.slug}></img>
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>£{product.price}</p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
