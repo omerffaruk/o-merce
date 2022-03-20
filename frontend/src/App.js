87182e45555dbcda7f55609bb43aabcd77637cb3
omer <omer@idevu.io>
2022-03-20 15:30:41 +0000

    feat: list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">o-merce</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.slug}></img>
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>£{product.price}</p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
