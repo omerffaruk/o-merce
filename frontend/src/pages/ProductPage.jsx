f74d457e34fee1826edb10a5e8862de32cf78be6
omer <omer@idevu.io>
2022-03-21 17:35:31 +0000

    feat: create product page using react-bootstrap, axios, useReducer, react-helmet, col, row, badge, listgroup

---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,111 @@
+import axios from 'axios';
+import React, { useEffect, useReducer } from 'react';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import Button from 'react-bootstrap/Button';
+import { Helmet } from 'react-helmet-async';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+function ProductPage() {
+  const params = useParams();
+  const { slug } = params;
+  const initialState = {
+    product: [],
+    loading: true,
+    error: '',
+  };
+  const [{ product, loading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_ERROR', payload: err });
+      }
+    };
+    fetchData();
+  }, [slug]);
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <Row>
+      <Col md={5}>
+        <img className="img-large" src={product.image} alt={product.slug}></img>
+      </Col>
+      <Col md={4}>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <Helmet>
+              <title>{product.name}</title>
+            </Helmet>
+            <h1>{product.name}</h1>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Rating rating={product.rating} numReviews={product.numReviews} />
+          </ListGroup.Item>
+          <ListGroup.Item>Price: £{product.price}</ListGroup.Item>
+          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col md={3}>
+        <Card>
+          <Card.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>${product.price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock >= 1 ? (
+                      <Badge bg="success">In Stock</Badge>
+                    ) : (
+                      <Badge bg="danger">Unavailable</Badge>
+                    )}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button className="primary">Add to cart</Button>
+                  </div>
+                </ListGroup.Item>
+              )}
+            </ListGroup>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
+export default ProductPage;
