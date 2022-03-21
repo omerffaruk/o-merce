adbe1fc8d6d8594ac05e43be2de85ca8fa19599c
omer <omer@idevu.io>
2022-03-21 13:59:58 +0000

    feat: create rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function Rating(props) {
+  const { rating, numReviews } = props;
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            rating >= 0.75
+              ? 'fas fa-star'
+              : rating >= 0.25
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 1.75
+              ? 'fas fa-star'
+              : rating >= 1.25
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2.75
+              ? 'fas fa-star'
+              : rating >= 2.25
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3.75
+              ? 'fas fa-star'
+              : rating >= 3.25
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4.75
+              ? 'fas fa-star'
+              : rating >= 4.25
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        ></i>
+      </span>
+      <span> {numReviews} reviews</span>
+    </div>
+  );
+}
+
+export default Rating;
