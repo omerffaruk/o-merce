0be5b52330187c3d46069036ece7fd19afd2bb93
omer <ffaruk.yamak@gmail.com>
2022-03-24 22:55:54 +0000

    refactor: prettier auto changes

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import Rating from './Rating';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 function Product(props) {
   const { product } = props;
