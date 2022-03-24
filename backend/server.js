628209aa120a853535c3f559db4dfc0417ba6b15
omer <omer@idevu.io>
2022-03-24 16:55:42 +0000

    refactor: turn one quote into two quotes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import data from "./data.js";
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
+  // res.sendStatus(404).send({ message: "No products found!" });
   res.send(data);
 });
 
-app.get('/api/products/slug/:slug', (req, res) => {
+app.get("/api/products/slug/:slug", (req, res) => {
   const product = data.products.find((p) => p.slug === req.params.slug);
   if (product) {
     return res.send(product);
   } else {
-    res.status(404).send({ message: 'Product Not Found' });
+    res.status(404).send({ message: "Product Not Found" });
   }
 });
 
