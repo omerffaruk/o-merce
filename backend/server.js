d0ea60c67e13fc0b70cd92492af1caadeec7385a
omer <ffaruk.yamak@gmail.com>
2022-03-27 16:27:15 +0100

    feat: connect to mongo db database with mongoose and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // loads variables from .env file and makes them available to the process
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const app = express();
 
 const port = process.env.PORT || 5000;
