cbcc1dd708ba45e97cd0fa11e6548eb773c24b49
omer <ffaruk.yamak@gmail.com>
2022-03-28 23:36:37 +0100

    feat: create mongoose schema

---
+++ backend/models/productModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+// create the schema first
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// then store the model in a variable
+const Product = mongoose.model("Product", productSchema);
+export default Product;
