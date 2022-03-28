3a355678489997f4d7d8055c2ea24223eef8e374
omer <ffaruk.yamak@gmail.com>
2022-03-28 23:57:03 +0100

    feat: create product router and import it in server js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRoutes from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config(); // loads variables from .env file and makes them available to the process
 mongoose
@@ -16,31 +17,9 @@ mongoose
 
 const app = express();
 app.use("/api/seed", seedRoutes);
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
-
-app.get("/api/products", (req, res) => {
-  // res.sendStatus(404).send({ message: "No products found!" });
-  res.send(data);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((p) => p.slug === req.params.slug);
-  if (product) {
-    return res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((p) => p._id === req.params.id);
-  if (product) {
-    return res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
 
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
