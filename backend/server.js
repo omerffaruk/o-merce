0aee26e9d3fc40af66fc4dfc455ede02fe33c859
omer <ffaruk.yamak@gmail.com>
2022-03-29 16:42:21 +0100

    feat: add users route, middleware for error handling

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRoutes from "./routes/seedRoutes.js";
-import productRouter from "./routes/productRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config(); // loads variables from .env file and makes them available to the process
 mongoose
@@ -16,8 +17,17 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/seed", seedRoutes);
-app.use("/api/products", productRouter);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ error: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
