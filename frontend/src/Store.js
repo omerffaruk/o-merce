9d365eb446ee808e7a7cbeafa6470361bc3430ea
omer <ffaruk.yamak@gmail.com>
2022-03-31 12:00:05 +0100

    feat: handle save shipping address and user signout with useContext and dispatch

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -7,6 +7,7 @@ const initialState = {
   userInfo: JSON.parse(localStorage.getItem("userInfo")) || null,
   cart: {
     cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
+    shippingAddress: JSON.parse(localStorage.getItem("shippingAddress")) || {},
   },
 };
 
@@ -38,7 +39,16 @@ function reducer(state, action) {
     case "USER_SIGNIN":
       return { ...state, userInfo: action.payload };
     case "USER_SIGNOUT":
-      return { ...state, userInfo: null };
+      return {
+        ...state,
+        userInfo: null,
+        cart: { cartItems: [], shippingAddress: {} },
+      };
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      };
     default:
       return state;
   }
