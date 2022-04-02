08de90391c41ac63502c75766b85947d0283ea6f
omer <ffaruk.yamak@gmail.com>
2022-04-02 18:05:11 +0100

    feat: create payment method case and initial state in useContext

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -8,6 +8,7 @@ const initialState = {
   cart: {
     cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
     shippingAddress: JSON.parse(localStorage.getItem("shippingAddress")) || {},
+    paymentMethod: localStorage.getItem("paymentMethod") || "",
   },
 };
 
@@ -42,12 +43,17 @@ function reducer(state, action) {
       return {
         ...state,
         userInfo: null,
-        cart: { cartItems: [], shippingAddress: {} },
+        cart: { cartItems: [], shippingAddress: {}, paymentMethod: "" },
       };
     case "SAVE_SHIPPING_ADDRESS":
       return {
         ...state,
         cart: { ...state.cart, shippingAddress: action.payload },
+      };
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
     default:
       return state;
