989e9e96c670dce0fd52ceaa9ef663e8f1c6c98c
omer <ffaruk.yamak@gmail.com>
2022-04-02 17:27:36 +0100

    feat: create sign up api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,7 +15,8 @@ userRouter.post(
       if (bcrypt.compareSync(password, user.password)) {
         res.send({
           _id: user._id,
-          name: user.name,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           isAdmin: user.isAdmin,
           token: generateToken(user),
@@ -24,6 +25,34 @@ userRouter.post(
       }
     }
     res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+userRouter.post(
+  "/signup",
+  expressAsyncHandler(async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).send({ message: "User already exists" });
+      return;
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.send({
+      _id: newUser._id,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: generateToken(newUser),
+    });
   })
 );
 
