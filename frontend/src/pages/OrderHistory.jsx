5dc857b4dd67150a41f950f1ae8ec77aa3858b2e
omer <ffaruk.yamak@gmail.com>
2022-04-10 12:57:07 +0100

    feat: create order history page

---
+++ frontend/src/pages/OrderHistory.jsx
@@ -0,0 +1,105 @@
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import React, { useContext, useEffect } from "react";
+import { useReducer } from "react";
+import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { Store } from "../Store";
+import { getError } from "../components/utils";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, orders: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+export default function OrderHistory() {
+  const navigate = useNavigate();
+  const [{ orders, loading, error }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const { data } = await axios.get(`/api/orders/mine`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        console.log(data);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(error) });
+      }
+    };
+    fetchOrders();
+  }, [userInfo]);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Order History</title>
+      </Helmet>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          <h1 className="my-3">Order History</h1>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>£{order.totalPrice.toFixed(2)}</td>
+                  <td>{order.isPaid ? order.paidAt.substring(0, 10) : "No"}</td>
+                  <td>
+                    {order.isDelivered
+                      ? order.deliveredAt.substring(0, 10)
+                      : "No"}
+                  </td>
+                  <td>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => {
+                        navigate(`/order/${order._id}`);
+                      }}
+                    >
+                      Details
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+}
